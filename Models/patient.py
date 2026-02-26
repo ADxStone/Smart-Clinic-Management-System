e3f46990ff79d9ab23810068b7a753987ac152bc
Mark Kamau <mark.wainaina@student.moringaschool.com>
2026-02-26 20:58:11 +0300

    GR8-2-Patients-Adds code from main.py.

--- Models/patient.py
+++ Models/patient.py
@@ -111,3 +111,38 @@ if __name__ == "__main__":
     Patient.delete_patients()
 
  
+
+
+#This Part of Code is From main.py
+from Models.patient import Patient
+
+def receptionist_menu():
+    while True:
+        print("\n--- Receptionist Menu ---")
+        print("1. Register Patient")
+        print("2. View Patients")
+        print("3. Search Patient")
+        print("4. Update Patient")
+        print("5. Delete Patient")
+        print("6. Back")
+
+        choice = input("Choose option: ")
+
+        if choice == "1":
+            Patient.register_patient()
+        elif choice == "2":
+            Patient.view_patients()
+        elif choice == "3":
+            Patient.search_patients()
+        elif choice == "4":
+            Patient.update_patients()
+        elif choice == "5":
+            Patient.delete_patients()
+        elif choice == "6":
+            break
+        else:
+            print("Invalid choice.")
+
+        
+if __name__ == "__main__":
+    receptionist_menu()
