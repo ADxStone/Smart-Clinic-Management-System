4fdae3673ba7ef1481ae23958cdc6d52f2b8fe50
jst.talel <mathewstalel@outlook.com>
2026-02-27 11:55:54 +0300

    GR8-5 Decorators: Completed merging of both the Users and Decorators code. Made some changes to existing main.py and users.py code.

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ from Utilities.decorators import login, role
 def doctor_menu(manager: UserManager, user=None):
     while True:
         print("\n--- DOCTOR MENU ---")
-        print("1. View my appointments")
-        print("2. Mark appointment completed (example)")
-        print("3. Back / Logout")
-        ch = input("Select (1-3): ").strip()
-        if ch == "1":
+        print("1. View my appointments- Not complete")
+        print("2. Mark appointment completed - Not complete")
+        print("3. View all complete appointments - Not complete")
+        print("4. Back / Logout")
+        choice = input("Select (1-3): ").strip()
+        if choice == "1":
             username = user.username if hasattr(user, 'username') else user.get('username')
             appts = list_appointments_for_doctor(username)
             print("Appointments for", username, appts)
-        elif ch == "2":
-            print("Example: mark completed (no storage logic implemented)")
-        elif ch == "3":
+        elif choice == "2":
+            print("Example: No logic implemented")
+        elif choice == "3":
+            print("Example: No logic implemented")
+        elif choice == "4":
             print(manager.logout_user())
             break
         else:
@@ -31,16 +34,19 @@ def doctor_menu(manager: UserManager, user=None):
 def receptionist_menu(manager: UserManager, user=None):
     while True:
         print("\n--- RECEPTIONIST MENU ---")
-        print("1. List patients")
-        print("2. Create appointment (example)")
-        print("3. Back / Logout")
-        ch = input("Select (1-3): ").strip()
-        if ch == "1":
+        print("1.List patients - Not complete ")
+        print("2.Create appointment-Not complete")
+        print("3.View appointments(1.For each patient, 2. For each doctor)-Not complete")
+        print("4. Back / Logout")
+        choice = input("Select (1-3): ").strip()
+        if choice == "1":
             pats = list_patients()
             print("Patients:", pats)
-        elif ch == "2":
-            print("Example: create appointment (no storage logic implemented)")
-        elif ch == "3":
+        elif choice == "2":
+            print("Example: create appointment (No logic implemented)")
+        elif choice == "3":
+            print("Example: View appointments(No logic implemented)")
+        elif choice == "4":
             print(manager.logout_user())
             break
         else:
